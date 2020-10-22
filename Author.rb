aab908fb23b6ca2599c7a9f72a8b9302d9359b27
Sourav Verma <sourav.verma@ttu.edu>
2020-10-22 07:20:09 -0700

    Delete Author.rb

--- Author.rb
+++
@@ -1,8 +0,0 @@
-require 'Sequel'
-
-class Author < Sequel::Model
-  Sequel::Model.plugin :json_serializer
-  set_primary_key :author_id
-  one_to_many :authors_books
-  many_to_many :books, join_table: :authors_books, left_key: :author_id, right_key: :book_id
-end
